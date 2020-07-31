b2bc47b2943af804dadbd897ea3bd02a7ca5c7e2
Talley Lambert <talley.lambert@gmail.com>
2020-07-31 12:54:05 -0400

    initial commit

---
+++ napari_s3zarr/s3zarr.py
@@ -0,0 +1,14 @@
+from napari_plugin_engine import napari_hook_implementation
+import dask.array as da
+
+
+@napari_hook_implementation
+def napari_get_reader(path):
+    if not isinstance(path, str):
+        return
+    if path.startswith("s3:") and path.endswith(".zarr"):
+        return reader_function
+
+
+def reader_function(path):
+    return [(da.from_zarr(path),)]
